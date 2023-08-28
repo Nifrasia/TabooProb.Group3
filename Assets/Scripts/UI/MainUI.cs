f672c9172451c494184c1a7ec58d9e9e71198e20
LittleT0fu <chinchanathavornkraiwit@gmail.com>
2023-08-28 22:20:06 +0700

    Buildpanel toogle button
    click button to open Buid panel and close

---
+++ Assets/Scripts/UI/MainUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainUI : MonoBehaviour
+{
+
+    public GameObject toolPanel;
+
+    public static MainUI instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void toggleToolPanel()
+    {
+     if(!toolPanel.activeInHierarchy){
+        toolPanel.SetActive(true);
+        }
+        else
+        {
+            toolPanel.SetActive(false); 
+        }
+    
+    
+    
+    }
+
+
+}
